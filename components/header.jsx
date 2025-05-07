990a8f8c37875b6517ccfc417af1c21745dc9090
Atharva Behani <atharva2204@gmail.com>
2025-05-07 18:17:53 +0530

    Integrated Convex Database with Clerk

--- components/header.jsx
+++ components/header.jsx
@@ -3,22 +3,26 @@
 import { useStoreUser } from "@/hooks/use-store-user";
 import { SignedOut, SignInButton, SignUpButton, UserButton, SignedIn } from "@clerk/nextjs";
 import React from "react";
+import { BarLoader } from "react-spinners";
 
 const Header = () => {
 
     const {isLoading} = useStoreUser();
-    
-    return (
-        <div>
-            <SignedOut>
-                <SignInButton />
-                <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-                <UserButton />
-            </SignedIn>
 
-        </div>
+    return (
+        <header className="fixed top-0 w-full border -b bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60">
+            <nav>
+                <SignedOut>
+                    <SignInButton />
+                    <SignUpButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </nav>
+
+            {isLoading && <BarLoader width={"100%"} color="#36d7b7" />}    
+        </header>
     );
   };
   
