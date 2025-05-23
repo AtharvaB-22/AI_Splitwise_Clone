4adb283c68d10d509c277a399152a1496deaed51
Atharva Behani <atharva2204@gmail.com>
2025-05-23 15:17:26 +0530

    Added 'Add Expense' API

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,6 @@
 import { inngest } from "@/lib/inngest/client";
 import { paymentReminders } from "@/lib/inngest/payment-reminders";
+import { spendingInsights } from "@/lib/inngest/spending-insights";
 import { serve } from "inngest/next";
 
 
@@ -7,5 +8,6 @@ export const { GET, POST , PUT } = serve({
     client: inngest,
     functions: [
         paymentReminders,
+        spendingInsights,
     ],
 });
